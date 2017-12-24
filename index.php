bfa9a3a30a6b3cc3646a564d0d1cbced95b009ab
Alan <alanbiegun13@gmail.com>
2017-12-24 14:12:21 +0100

    Usunięcie zbędnego pliku

--- index.php
+++ index.php
@@ -3,7 +3,20 @@
 
 ob_start();
 echo "<head>";
-include_once("Views/Shared/Includes.php");
+echo "
+    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
+
+    <link href=\"/Styles/font-awesome.min.css\" rel=\"stylesheet\">
+    <link href=\"/Styles/bootstrap.css\" rel=\"stylesheet\">
+    <link href=\"/Styles/footer.css\" rel=\"stylesheet\">
+    <link href=\"/Styles/Custom.css\" rel=\"stylesheet\">
+    <link href=\"/Styles/menu.css\" rel=\"stylesheet\">
+
+    <script src=\"/Scripts/jquery-1.10.2.js\"></script>
+    <script src=\"/Scripts/bootstrap.js\"></script>
+    <script src=\"/Scripts/Custom/CookieAccept.js\"></script>
+    <script src=\"/Scripts/Custom/MenuSubcategories.js\"></script>
+";
 include_once("Config/DatabaseContext.php");;
 //include_once("Views/Shared/Menu.php");
 include_once("Config/route.php");
@@ -27,7 +40,7 @@ echo "</head>";
 echo "<body>";
 
 echo "<nav id=\"Header\" class=\"navbar navbar-default\" style=\"margin-bottom: 0px;\">";
-
+include_once("Views/Shared/Menu.php");
 echo "</nav>
 <div id=\"MainContainerModal\">
     <div id=\"MainModalContent\" class=\"container\"></div>
