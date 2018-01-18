bbd3294f824abfd127975d718bf21d596f3654cd
Alan <alanbiegun13@gmail.com>
2018-01-18 19:40:08 +0100

    Poprawki styli

--- Views/Shared/_AdministrationBar.php
+++ Views/Shared/_AdministrationBar.php
@@ -1,11 +1,8 @@
-<div class="container navbar-fixed-top" style="width: 1170px">
+<div class="container navbar-fixed-top" style="width: 1280px">
 <nav class="navbar navbar-default ">
     <div class="container-fluid">
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-
-
             <ul class="nav navbar-nav navbar-left">
                 <li><a href="">Strona główna</a></li>
                 <li class="active"><a>Zalogowany jako {administrator}</a></li>
