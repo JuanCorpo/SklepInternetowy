987adba2cae85a18f071f0b1fdce2f0c49c5f7f7
Juan <dawidof11@gmail.com>
2018-01-18 23:39:46 +0100

    Poprawiony widok

--- Views/Products/Show.php
+++ Views/Products/Show.php
@@ -9,15 +9,24 @@ function ProductsShow($model)
 
     <div class="col-md-12">
     <a style='display:inline;'> <h1 style='display:inline;'> <?php echo $model->Name; ?> </h1> </a>
+        <?php echo '<div style="display: inline;" id="stars_' . $model->ProductId . '" stars="' . $model->GetRating() . '" class="starrr"></div>
+        <div style="display:inline">(' . $model->NoOfRatings . ')</div>' ;?>
+        <button class="btn btn-warning navbar-right" onclick="addToBasket(<?php echo $model->ProductId; ?>)"><span class="glyphicon glyphicon-plus"></span> Dodaj do koszyka </button>
         <?php if (RoleHelper::IsInRole(1)) { ?>
-    <?php echo '<a href="/Administration/EditProduct/'.$model->ProductId.'" <button class="btn btn-warning navbar-right"><span class="glyphicon glyphicon-pencil"></span> Edytuj produkt </button></a>';
+    <?php echo '<a href="/Administration/EditProduct/'.$model->ProductId.'" <button class="btn btn-warning navbar-right" style="margin-right:10px"><span class="glyphicon glyphicon-pencil"></span> Edytuj produkt </button></a>';
          } ?>
         <hr>
     </div>
 
     <div class="col-md-12">
+        <div class="col-md-6">
         <?php  echo $model->Description; ?>
+        </div>
+        <div class="col-md-6">
+        <img style="max-height: 300px; max-width: 500px" src="<?php echo $model->ImageDirectory; ?>"/>
+        </div>
     </div>
+
 
     <div class="col-md-12">
         <br>
@@ -44,7 +53,8 @@ function ProductsShow($model)
 </table>
 </div>
 
-
+        <script src="/Scripts/Custom/Stars.js"></script>
+        <script src="/Scripts/ajax/Basket.js"></script>
     </div>
 
     <?php
